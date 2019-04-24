80c70e03f4d7925f5a4c19952bf24b2e65463888
langeslet <marlon.valentin@outlook.com>
2019-04-24 20:21:22 +0800

    Add project files

---
+++ ConnectSPO/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConnectSPO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
